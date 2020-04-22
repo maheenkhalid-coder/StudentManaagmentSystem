8c48f25c1aeedf9f3a9784fbdb11337885c87346
dell <dell@DESKTOP-O6LSKAG>
2020-04-22 19:55:09 +0500

    Test

--- src/com/sms/application/Main.java
+++ src/com/sms/application/Main.java
@@ -6,6 +6,6 @@ public class Main {
 //        Project: Student Management System
 //        The student management system allows users to keep track of students' information, courses, exams and fees.
 //        Create the possible classes and relationship between these classes.(Use OOP concepts)
-        System.out.println("Tesst");
+        System.out.println("Tesst b");
     }
 }
