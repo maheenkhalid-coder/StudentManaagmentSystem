baa786037e3da5593c1b1151a80534b0e3d4a2f6
Junaid Mohiuddin <junaid.mohiuddin@sap.com>
2020-04-22 15:42:40 +0200

    Initial Commit

---
+++ src/com/sms/application/Main.java
@@ -0,0 +1,10 @@
+package com.sms.application;
+
+public class Main {
+
+    public static void main(String[] args) {
+//        Project: Student Management System
+//        The student management system allows users to keep track of students' information, courses, exams and fees.
+//        Create the possible classes and relationship between these classes.(Use OOP concepts)
+    }
+}
