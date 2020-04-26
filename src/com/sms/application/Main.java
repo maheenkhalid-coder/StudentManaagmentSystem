3800661f2ce27b94abba23886a2e6e4fd8745fb3
dell <dell@DESKTOP-O6LSKAG>
2020-04-26 15:39:07 +0500

    Add CRUD operations for Student!!

--- src/com/sms/application/Main.java
+++ src/com/sms/application/Main.java
@@ -1,89 +1,99 @@
 package com.sms.application;
 
-
 import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 //        Project: Student Management System
 //        The student management system allows users to keep track of students' information, courses, exams and fees.
 //        Create the possible classes and relationship between these classes.(Use OOP concepts)
-    	
-    	int x=0;
-    	int code;
-    	StudentInfo[] students = new StudentInfo[1];
-    	
-    	do {
-    	
-    	System.out.println("\t\t\tWELCOME TO THE PROJECT MANAGEMENT SYSTEM OF STUDENTS\t\t");
-    	System.out.println("Press 1\t For Enroll new Student.\nPress 2\t For See the list.\nPress 3\t For Quit.");
-    	 	
-    	
-        Scanner function = new Scanner(System.in);
-		code = function.nextInt();
-		switch(code)
-		{
-    	
-    	case 1 :
-    	
-    		System.out.println("\t\t\tPLEASE FOLLOW THE ENROLLMENT PROCESS");
-        	//Scanner in = new Scanner(System.in);
-        	//TotalStudents = in.nextInt();
-        		
-        	for (int n = 0; n < 1; n++) {
-        		students[n] = new StudentInfo();
-        		students[n].Student();
-        		students[n].courseEnrollment();
-        		students[n].paidPayment();
-        		students[n].AboutMakrs();
-        		students[n].AboutPassorFail();
-        		students[n].resultfinal();
-        		System.out.println("-------xxxx------");
-        		System.out.println("\n");
-        		System.out.println("Your Final Info:");
-        		System.out.printf(students[n].toFinalInfo());
-        		students[n].resultfinal();
-        	}
-        
-        break;
-        
-    	case 2:
-    		
-    	System.out.println("\t\t\tTHESE ARE THE STUDENTS THAT ENROLLED");
-    	StudentInfo stu1 = new StudentInfo();
-    	stu1.DumyData();
-    	System.out.println("\t\t\tNEW STUDENT ADDED");
-    	
-    	for(int i=0;i<1;i++)
-    	{
-    				
-    		System.out.println(students[i].toFinalInfo());			
-    	
-    	}
-    	
-    	break;
-    	
-    	default :
-    		System.out.println("Thanks for visiting!!");
-    	}
-        	
-		System.out.println("\nPress M for Main menu");
-		Scanner Mainmenu = new Scanner(System.in);
-		String Value = Mainmenu.nextLine();
-        	
-    	}
-    	
-    	while(x<10000);
 
-    	}
-    	
-    }
-    	
+		boolean isExit = false;
+		int code;
+		StudentInfo[] studentArray = new StudentInfo[100];
+		int numberOfStudent = 0;
 
+		Courses[] coursesArray = new Courses[100];
+		int numberofCourses = 0;
 
-    	
+		do {
 
+			System.out.println("\t\t\tWELCOME TO THE PROJECT MANAGEMENT SYSTEM OF STUDENTS\t\t");
+			System.out.println(
+					"Press 1\t For Enroll new Student.\nPress 2\t For See the Student list.\nPress 3\t For add courses.\nPress 4\t List of all added courses.\nPress 5\t For Examination Process.\nPress 6\t For Fees Structure.\nPress 7\t For Quit.");
 
-    	
-  
+			Scanner scanner = new Scanner(System.in);
+			code = scanner.nextInt();
+			switch (code) {
+
+			case 1:
+
+				System.out.println("\t\t\tPLEASE FOLLOW THE ENROLLMENT PROCESS");
+
+				StudentInfo student = new StudentInfo();
+				student = new StudentInfo();
+				student.Student();
+				System.out.println("\n");
+				studentArray[numberOfStudent++] = student;
+
+				break;
+
+			case 2:
+
+				System.out.println("\t\t\tTHESE ARE THE STUDENTS THAT ENROLLED");
+
+				for (int i = 0; i < numberOfStudent; i++) {
+
+					System.out.println(studentArray[i].toFinalInfo());
+					System.out.println("\n");
+
+				}
+
+				break;
+
+			case 3:
+				Courses addition = new Courses();
+				addition.CoursesAddition();
+				System.out.println("\n");
+
+				coursesArray[numberofCourses++] = addition;
+				break;
+
+			case 4:
+
+				System.out.println("\t\t\tTHESE ARE THE COURSES THAT ADDED");
+				for (int a = 0; a < numberofCourses; a++) {
+					System.out.println(coursesArray[a].toCourseInfo());
+					System.out.println("\n");
+
+				}
+				break;
+			case 5:
+				System.out.println("\t\t\tEXAMINATION PROCESS");
+				Courses Exams = new Courses();
+				Exams.CoursesAddition();
+				Exams.AboutPassorFail();
+				Exams.resultfinal();
+				break;
+
+			case 6:
+				System.out.println("\t\t\tFEES STRUCTURE");
+				Courses Fees = new Courses();
+				Fees.toCourseInfo();
+				Fees.Fees();
+				System.out.println("\n");
+				break;
+
+			case 7:
+				isExit = true;
+				break;
+			default:
+
+			}
+
+		} while (!isExit);
+
+	}
+
+}
--- src/com/sms/application/StudentInfo.java
+++ src/com/sms/application/StudentInfo.java
@@ -2,142 +2,54 @@ package com.sms.application;
 
 import java.util.Scanner;
 
-public class  StudentInfo {
+public class StudentInfo {
 
 	private String firstName;
 	private String Lastname;
 	private int StudentID;
-	private String courses = "";
-	private static int tutionfee = 5000;
-	private  static int costofCourse = 1000;
-	private int BlanceAmout;
-	private static int id = 1000;
-	private static int totalMArks = 500;
-	private static int MarksperCourse = 100;
-	private int YourMarks;
-	private int English;
-	private int Urdu;
-	private int Pst;
-	private int Math;
-	private int Java;
-	private int sum;
+	private String Department;
+	private String Section;
 
-	
-	
 	public void Student() {
 
 		Scanner in = new Scanner(System.in);
-		System.out.print("Enter Student First Name: ");
+		System.out.print("Enter your First Name: ");
 		this.firstName = in.nextLine();
-		
-		System.out.print("Enter Student Last Name: ");
-		this.Lastname = in.nextLine();
-		
-		System.out.print("Enter Student ID: ");
-		this.StudentID = in.nextInt();
-		
-		setStudentID();  
-			
-	}
-	private void setStudentID() {
-		id++;
-		this.StudentID = id;
-		
-	}
-	
-	public void courseEnrollment() {
-		
-	System.out.println("Enter your Courses:");
-	Scanner course = new Scanner(System.in);
-	String course1 = course.nextLine();
-	String course2 = course.nextLine();
-	String course3 = course.nextLine();
-	String course4 = course.nextLine();
-	String course5 = course.nextLine();
 
-		}
-		
-	
-	public void BlanceAmount() {
-		System.out.println("Your Total Amount is: Rs: 5000");
-		
+		System.out.print("Enter your Last Name: ");
+		this.Lastname = in.nextLine();
+
+		System.out.print("Enter your Student ID: ");
+		this.StudentID = in.nextInt();
+
+		System.out.println("Enter your Department ");
+		this.Department = in.nextLine();
+
+		System.out.println("  and Section: ");
+		this.Section = in.nextLine();
+
 	}
-		
-	public void paidPayment() {
-		BlanceAmount();
-		System.out.println("Enter your payemnt Rs:");
-		Scanner in = new  Scanner(System.in);
-		int payment = in.nextInt();
-		tutionfee = tutionfee - payment;
-		System.out.println   ("Thank you for your payemnt Rs:" + payment);
-		System.out.println("Your Blance Amount is Rs: " +tutionfee);
-			
-	}
-	
-	public void AboutMakrs() {
-		
-		System.out.println("\t\t\tEXAM PROCESS");
-		System.out.println("Enter your ENGLISH course marks: ");
-		Scanner code = new Scanner(System.in);
-		this.English = code.nextInt();
-		System.out.println("Enter your URDU course marks: ");
-		this.Urdu = code.nextInt();		
-		System.out.println("Enter your PST course marks: ");
-		this.Pst = code.nextInt();
-		System.out.println("Enter your MATH course marks: ");
-		this.Math = code.nextInt();
-		System.out.println("Enter your JAVA course marks: ");
-		this.Java = code.nextInt();
-		
-		sum = English + Urdu + Pst + Math + Java;
-		
-		
-	}
-	
-	public void AboutPassorFail() {
-		
-		System.out.println("Your Total Marks are: " + sum);
-		
-		System.out.println("\n");
-	}
-	
-		public void resultfinal() {
-	    
-	    if(sum > 250) {
-	   
-	    	
-	    	System.out.println("Your Final result is: \nPASS");
-	    	System.out.println("\n");
-	    }
-	    else {
-	    	System.out.println("Your Final result is: \nFAil");
-	    	System.out.println("\n");
-	    	
-	    }    		
-	}
-	
+
 	public String toFinalInfo() {
 		System.out.println("\n");
-		return ("Name: " + firstName + " " + Lastname + "\nStudentID: " + StudentID + "\nCourses Enrolled:\n1.English.\n2.Urdu.\n3.Java.\n4.Pst.\n5.Math.\n");
-		
-			
-		}
+		return ("Name: " + firstName + " " + Lastname + "\nStudentID: " + StudentID + "\nSection and Department: "
+				+ Section);
+
+	}
+
 	public void DumyData() {
-	
-		System.out.println("Name: Sara Khan\nStudentID: 1009\nCourses Enrolled:\n1.English\n2.Urdu\n3.Math\n4.Java\n5.Pst.");
-		System.out.println("\n");
-		System.out.println("Name: Maria Rohail\nStudentID: 1002\nCourses Enrolled:\n1.English\n2.Urdu\n3.Math\n4.Java\n5.Pst.");
-		System.out.println("\n");
-		System.out.println("Name: Ali Khan\nStudentID: 1003\nCourses Enrolled:\n1.English\n2.Urdu\n3.Math\n4.Java\n5.Pst.");
-		System.out.println("\n");
-		System.out.println("Name: Saana Emaan\nStudentID: 1006\nCourses Enrolled:\n1.English\n2.Urdu\n3.Math\n4.Java\n5.Pst.");
-	    }
-	
-		}
-	
 
-		
+		System.out.println(
+				"Name: Sara Khan\nStudentID: 1009\nCourses Enrolled:\n1.English\n2.Urdu\n3.Math\n4.Java\n5.Pst.");
+		System.out.println("\n");
+		System.out.println(
+				"Name: Maria Rohail\nStudentID: 1002\nCourses Enrolled:\n1.English\n2.Urdu\n3.Math\n4.Java\n5.Pst.");
+		System.out.println("\n");
+		System.out.println(
+				"Name: Ali Khan\nStudentID: 1003\nCourses Enrolled:\n1.English\n2.Urdu\n3.Math\n4.Java\n5.Pst.");
+		System.out.println("\n");
+		System.out.println(
+				"Name: Saana Emaan\nStudentID: 1006\nCourses Enrolled:\n1.English\n2.Urdu\n3.Math\n4.Java\n5.Pst.");
+	}
 
-	
-	
-	
+}
